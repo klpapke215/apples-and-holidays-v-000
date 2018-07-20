4c3cb1f2ea3dff26c60ad81fcb786c4faa133f48
Kristen Papke <klpapke215@gmail.com>
2018-07-20 16:21:30 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash[].values.flatten 
+  holiday_hash(supplies).values.flatten 
 
 end
 

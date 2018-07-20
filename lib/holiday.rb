454bb275c26e70eedbc69983cfb1dcbd71e98359
Kristen Papke <klpapke215@gmail.com>
2018-07-20 16:05:22 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -24,8 +24,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-  holiday_hash[:winter].collect do |balloons|
-    holiday_hash[:winter] = "balloons"
+  holiday_hash[:winter].each do |holiday, decorations|
+    decorations << supply 
   end
 end
 
